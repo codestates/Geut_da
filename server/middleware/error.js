dc9cde00c27b04e7726aab983ab89c6664fdf66a
developerjhp <developerjhp@gmail.com>
2021-11-11 17:12:57 +0900

    [Feat] #11 미들웨어 구현 closed #11

---
+++ server/middleware/error.js
@@ -0,0 +1,20 @@
+// err custom handler
+
+const notFound = (req, res, next) => {
+  const err = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(err);
+};
+
+const errHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+// err인데 200 코드가 난다면 500으로 바꾸기
+// 개발 환경일 경우에만 스텍 보여주기
+
+module.exports = { notFound, errHandler };
