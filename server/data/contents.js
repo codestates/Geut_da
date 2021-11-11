88026f28debd13601d824396be15b72312c2802d
Jin garam <2001garam@naver.com>
2021-11-11 23:03:41 +0900

    [Feat] #13 서버 유저파트 기능 구현

--- server/data/contents.js
+++ server/data/contents.js
@@ -1,32 +1,31 @@
 const contents = [
   {
-    title: "바다 간 날",
-    text: "회는 추울때 먹어야 제 맛",
-    weather: "cloud",
+    title: '바다 간 날',
+    text: '회는 추울때 먹어야 제 맛',
+    weather: 'cloud',
     drawing: {
-      url: "https://picsum.photos/200"
+      url: 'https://picsum.photos/200',
     },
-    hashtag: ['바다','회'],
+    hashtag: ['바다', '회'],
   },
   {
-    title: "산 간 날",
-    text: "더워 ",
-    weather: "sun",
+    title: '산 간 날',
+    text: '더워 ',
+    weather: 'sun',
     drawing: {
-      url: "https://picsum.photos/200"
+      url: 'https://picsum.photos/200',
     },
-    hashtag: ['산','더워'],
+    hashtag: ['산', '더워'],
   },
   {
-    title: "섬 간 날",
-    text: "낙지는 추울때 먹어야 제 맛",
-    weather: "snow",
+    title: '섬 간 날',
+    text: '낙지는 추울때 먹어야 제 맛',
+    weather: 'snow',
     drawing: {
-      url: "https://picsum.photos/200"
+      url: 'https://picsum.photos/200',
     },
-    hashtag: ['섬','배'],
+    hashtag: ['섬', '배'],
   },
 ];
 
-// export default posts;
 export default contents;
