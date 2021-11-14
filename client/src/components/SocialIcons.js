e675979401b3901703b2a945c373915f37020395
Jin garam <2001garam@naver.com>
2021-11-14 17:54:58 +0900

    [Fix] #93 소설 아이콘 링크

---
+++ client/src/components/SocialIcons.js
@@ -0,0 +1,46 @@
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import styled from 'styled-components/macro';
+
+export const StyledSocialIcons = styled.div`
+  li {
+    list-style: none;
+  }
+
+  a {
+    border: 2px solid #cd5c5c;
+    border-radius: 50%;
+    color: #cd5c5c;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    margin-right: 10px;
+    height: 40px;
+    width: 40px;
+    text-decoration: none;
+    &:hover {
+      color: #daa520;
+      border-color: #daa520;
+    }
+  }
+`;
+
+const SocialIcons = () => {
+  return (
+    <StyledSocialIcons>
+      <li>
+        <a href='https://facebook.com/'>
+          <FaFacebook />
+        </a>
+        <a href='https://twitter.com/'>
+          <FaTwitter />
+        </a>
+        <a href='https://instagram.com/'>
+          <FaInstagram />
+        </a>
+      </li>
+    </StyledSocialIcons>
+  );
+};
+
+export default SocialIcons;
