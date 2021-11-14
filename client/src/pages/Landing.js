d487acedbba8d36127d32dc841dfae0689e5b7bb
developerjhp <developerjhp@gmail.com>
2021-11-14 20:02:55 +0900

    [Feat] #96 로그인여부에 따른 라우트접근 제한 기능 구현

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,9 +3,6 @@ import Header from '../components/Header';
 import LoginModal from '../components/Modal/LoginModal';
 import SignupModal from '../components/Modal/SignupModal';
 import styled from 'styled-components';
-import IsLoginState from '../states/IsLoginState';
-import { useRecoilValue } from 'recoil';
-import { useNavigate } from 'react-router-dom';
 
 const ModalBackDrop = styled.div`
   position: fixed;
@@ -20,8 +17,6 @@ const ModalBackDrop = styled.div`
 const Landing = () => {
   const [clickLogin, setClickLogin] = useState(false);
   const [clickSignup, setClickSignup] = useState(false);
-  const loginCheck = useRecoilValue(IsLoginState);
-  const history = useNavigate();
 
   const LoginModalHandler = () => {
     setClickLogin(!clickLogin);
@@ -31,11 +26,6 @@ const Landing = () => {
     setClickSignup(!clickSignup);
   };
 
-  useEffect(() => {
-    if (loginCheck) {
-      history('/main');
-    }
-  });
 
   return (
     <>
