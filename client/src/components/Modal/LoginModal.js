05cca4e65c7c80a2e8afce189bada7477d5d92c7
linehyang <83056347+linehyang@users.noreply.github.com>
2021-11-15 23:54:00 +0900

    [Feat] #118 Login-CSS (#119)
    - Login 버튼 클릭뿐만이 아닌 엔터를 눌러서도 로그인 요청이 가능하게끔 수정
    - 로그인 핸들링 문구 CSS적용(에러는 빨강색, 정상 초록색)
    - 로그인 페이지 CSS로 꾸미기

--- client/src/components/Modal/LoginModal.js
+++ client/src/components/Modal/LoginModal.js
@@ -7,33 +7,93 @@ import IsLoginState from '../../states/IsLoginState';
 
 const LoginModalWrap = styled.div`
   display: flex;
+  position: relative;
   width: 70%;
   height: 70vh;
-  margin: 15em auto;
-  border: 1px solid black;
+  background-color: #ffffff;
+
+  .closeButton {
+    position: absolute;
+    top: 1%;
+    right: 1%;
+    border: none;
+    color: #646464;
+    background-color: #ffffff;
+    font-weight: 700;
+    font-size: 1.5em;
+  }
+  .closeButton:hover {
+    cursor: pointer;
+  }
 `;
 
 const LoginModalImg = styled.div`
   width: 50%;
   height: 100%;
-  border: 1px solid black;
+  border-right: 1px solid black;
 `;
 
 const LoginModalContentWrap = styled.div`
   width: 50%;
   height: 100%;
   text-align: center;
-  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
+const LoginModalSubContentWrap = styled.div`
+  width: 70%;
+  height: 85%;
+  border-radius: 10px;
+  box-shadow: 23px 23px 46px #d9d6d6, -23px -23px 46px #ffffff;
+`;
 const LoginModalInputWrap = styled.div`
   display: flex;
   flex-direction: column;
+
+  input {
+    border: none;
+    height: 1.2em;
+    border-bottom: 1px solid #c4c4c4;
+    text-align: center;
+    font-size: 1.4em;
+    margin: 0.3em 3em;
+  }
+  input:focus {
+    outline: none;
+  }
+  span {
+    color: red;
+  }
 `;
 
 const LoginModalLinkWrap = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
+
+  button {
+    width: 18em;
+    height: 3em;
+    border: none;
+    border-radius: 10px;
+    background-color: #9e9e9e;
+    color: #ffffff;
+    font-weight: 700;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  span {
+    margin: 0.6em 0;
+  }
+  span:hover {
+    color: blue;
+    border-bottom: 1px solid blue;
+  }
 `;
 
 const LoginModal = ({ LoginModalHandler, SignupModalHandler }) => {
@@ -63,7 +123,7 @@ const LoginModal = ({ LoginModalHandler, SignupModalHandler }) => {
     SignupModalHandler();
   };
 
-  const LogInReqHandler = async (event) => {
+  const LogInReqHandler = (event) => {
     const config = {
       headers: {
         'Content-Type': 'application/json',
@@ -102,6 +162,12 @@ const LoginModal = ({ LoginModalHandler, SignupModalHandler }) => {
     }
   };
 
+  const LogInReqEnterHandler = (event) => {
+    if (event.key === 'Enter') {
+      LogInReqHandler(loginInputInfo);
+    }
+  };
+
   const LoginModalCloseHandler = () => {
     LoginModalHandler(false);
   };
@@ -109,35 +175,41 @@ const LoginModal = ({ LoginModalHandler, SignupModalHandler }) => {
   return (
     <LoginModalWrap onClick={(e) => e.stopPropagation()}>
       <LoginModalImg>이미지</LoginModalImg>
+      <button onClick={LoginModalCloseHandler} className={'closeButton'}>
+        &times;
+      </button>
       <LoginModalContentWrap>
-        <button onClick={LoginModalCloseHandler}>&times;</button>
-        <div>로고</div>
-        <LoginModalInputWrap>
-          <div>Login</div>
-          <input
-            type='email'
-            placeholder='Email'
-            value={loginInputInfo.email}
-            onChange={InputValueChangeHandler}
-          />
-          <input
-            type='password'
-            placeholder='password'
-            value={loginInputInfo.password}
-            onChange={InputValueChangeHandler}
-          />
-          {loginMessage ? (
-            <span>
-              &#42;아이디 또는 비밀번호가 잘못 입력 되었습니다.
-              <br />
-              <b>아이디</b>와 <b>비밀번호</b>를 정확히 입력해 주세요.
-            </span>
-          ) : null}
-        </LoginModalInputWrap>
-        <LoginModalLinkWrap>
-          <span onClick={OpenSignUpModalHandler}>아직 계정이 없습니까?</span>
-          <button onClick={LogInReqHandler}>Login</button>
-        </LoginModalLinkWrap>
+        <LoginModalSubContentWrap>
+          <div>로고</div>
+          <LoginModalInputWrap>
+            <div>Login</div>
+            <input
+              type='email'
+              placeholder='Email'
+              value={loginInputInfo.email}
+              onChange={InputValueChangeHandler}
+              onKeyPress={LogInReqEnterHandler}
+            />
+            <input
+              type='password'
+              placeholder='password'
+              value={loginInputInfo.password}
+              onChange={InputValueChangeHandler}
+              onKeyUp={LogInReqEnterHandler}
+            />
+            {loginMessage ? (
+              <span>
+                &#42;아이디 또는 비밀번호가 잘못 입력 되었습니다.
+                <br />
+                <b>아이디</b>와 <b>비밀번호</b>를 정확히 입력해 주세요.
+              </span>
+            ) : null}
+          </LoginModalInputWrap>
+          <LoginModalLinkWrap>
+            <span onClick={OpenSignUpModalHandler}>아직 계정이 없습니까?</span>
+            <button onClick={LogInReqHandler}>Login</button>
+          </LoginModalLinkWrap>
+        </LoginModalSubContentWrap>
       </LoginModalContentWrap>
     </LoginModalWrap>
   );
