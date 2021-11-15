bf037f476fb1d5033139871920012c6cf350e8d0
linehyang <83056347+linehyang@users.noreply.github.com>
2021-11-15 18:05:07 +0900

    [Fix] #115 Mypage 회원정보 수정 (#117)
    - 회원정보 수정시, 비밀번호나 닉네임 하나만 바꾸는것이 불가능한 현상
    - 새로운 패스워드 입력창에 기존 사용중인 비밀번호를 다시 입력 가능한 현상
    - mypage 모달창 on/off 기능
    - 회원탈퇴 후 랜딩페이지로 이동하지 않는 현상

--- client/src/components/Modal/LeaveModal.js
+++ client/src/components/Modal/LeaveModal.js
@@ -1,6 +1,17 @@
+import styled from 'styled-components';
+
+const LeaveModalWrap = styled.div`
+  display: flex;
+  width: 50%;
+  height: 50vh;
+  margin: 15em auto;
+  border: 1px solid black;
+  flex-direction: column;
+`;
+
 const LeaveModal = ({ isUserResignHandler, isRealUserResignHandler }) => {
   return (
-    <>
+    <LeaveModalWrap onClick={(e) => e.stopPropagation()}>
       <button onClick={isUserResignHandler}>Close</button>
       <div>
         탈퇴시 모든 게시물이 삭제됩니다.
@@ -9,7 +20,7 @@ const LeaveModal = ({ isUserResignHandler, isRealUserResignHandler }) => {
       </div>
       <button onClick={isRealUserResignHandler}>YES</button>
       <button onClick={isUserResignHandler}>NO</button>
-    </>
+    </LeaveModalWrap>
   );
 };
 
--- client/src/components/Modal/UserEditModal.js
+++ client/src/components/Modal/UserEditModal.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const UserEditModal = ({ openUserEditModalHandler }) => {
+const UserEditModalWrap = styled.div`
+  display: flex;
+  width: 50%;
+  height: 50vh;
+  margin: 15em auto;
+  border: 1px solid black;
+  flex-direction: column;
+`;
+
+const UserEditModal = ({ openUserEditModalHandler, pwCheckdValue }) => {
   const [userInputInfo, setUserInputInfo] = useState({
     nickname: '',
     password: '',
@@ -12,7 +22,7 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
     password: false,
     passwordCheck: false,
   });
-  const [allValidateCheck, setAllValidateCheck] = useState(false);
+
   const [nicknameValidateMessage, setNicknameValidateMessage] = useState(false);
   const [nicknameValidText, setNicknameValidText] = useState(false);
   const [pwValidateMessage, setPwValidateMessage] = useState(false);
@@ -27,38 +37,42 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
     } else if (!editValidateState.nickname && nicknameValidText === 'overlap') {
       setNicknameValidateMessage('사용중인 닉네임 입니다');
     } else if (!editValidateState.nickname && nicknameValidText === 'nothing') {
-      setNicknameValidateMessage('필수 입력 사항입니다');
-    } else if (!editValidateState.nickname && nicknameValidText === 'invalidate') {
+      setNicknameValidateMessage('변경하실 닉네임을 적어주세요');
+    } else if (
+      !editValidateState.nickname &&
+      nicknameValidText === 'invalidate'
+    ) {
       setNicknameValidateMessage('두글자 이상의 닉네임을 입력해주세요');
     }
 
     //비밀번호
-    if (editValidateState.password && pwValidText === 'ok') {
+    if (!editValidateState.password && pwValidText === 'same') {
+      setPwValidateMessage('기존과 동일한 비밀번호 입니다');
+    } else if (editValidateState.password && pwValidText === 'ok') {
       setPwValidateMessage('사용 가능한 비밀번호 입니다');
     } else if (!editValidateState.password && pwValidText === 'nothing') {
-      setPwValidateMessage('필수 입력 사항입니다');
+      setPwValidateMessage('변경하실 비밀번호를 적어주세요');
     } else if (!editValidateState.password && pwValidText === 'invalidate') {
-      setPwValidateMessage('알파벳, 숫자, 특수문자를 포함하여 8~20글자를 입력해주세요.');
+      setPwValidateMessage(
+        '알파벳, 숫자, 특수문자를 포함하여 8~20글자를 입력해주세요.'
+      );
     }
 
     //비밀번호 확인
     if (editValidateState.passwordCheck && pwCheckValidText === 'ok') {
       setPwCheckValidateMessage('비밀번호 일치');
-    } else if (!editValidateState.passwordCheck && pwCheckValidText === 'nothing') {
-      setPwCheckValidateMessage('필수 입력 사항입니다');
-    } else if (!editValidateState.passwordCheck && pwCheckValidText === 'invalidate') {
+    } else if (
+      !editValidateState.passwordCheck &&
+      pwCheckValidText === 'nothing'
+    ) {
+      setPwCheckValidateMessage('비밀번호 변경시 필수 입력 사항입니다');
+    } else if (
+      !editValidateState.passwordCheck &&
+      pwCheckValidText === 'invalidate'
+    ) {
       setPwCheckValidateMessage('비밀번호와 일치하지 않습니다');
     }
   }, [nicknameValidText, pwValidText, pwCheckValidText]);
-
-  useEffect(() => {
-    //editValidateState 변경시 마다 실행.
-    if (editValidateState.password === true && editValidateState.passwordCheck === true && editValidateState.nickname === true) {
-      setAllValidateCheck(true);
-    } else {
-      setAllValidateCheck(false);
-    }
-  }, [editValidateState]);
 
   const pwdExp = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
   const nicknameExp = /^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{1,10}$/;
@@ -69,7 +83,9 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
   };
   const config2 = {
     headers: {
-      Authorization: `Bearer ${JSON.parse(localStorage.getItem('userInfo')).token}`,
+      Authorization: `Bearer ${
+        JSON.parse(localStorage.getItem('userInfo')).token
+      }`,
     },
   };
 
@@ -79,10 +95,16 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
       setUserInputInfo({ ...userInputInfo, nickname: event.target.value });
     }
 
-    if (event.target.type === 'password' && event.target.placeholder === 'New Password') {
+    if (
+      event.target.type === 'password' &&
+      event.target.placeholder === 'New Password'
+    ) {
       setUserInputInfo({ ...userInputInfo, password: event.target.value });
     }
-    if (event.target.type === 'password' && event.target.placeholder === 'Password Check') {
+    if (
+      event.target.type === 'password' &&
+      event.target.placeholder === 'Password Check'
+    ) {
       setUserInputInfo({
         ...userInputInfo,
         passwordCheck: event.target.value,
@@ -95,12 +117,22 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
     if (event.target.type === 'text' && userInputInfo.nickname === '') {
       setEditValidateState({ ...editValidateState, nickname: false });
       setNicknameValidText('nothing');
-    } else if (event.target.type === 'text' && !nicknameExp.test(userInputInfo.nickname)) {
+    } else if (
+      event.target.type === 'text' &&
+      !nicknameExp.test(userInputInfo.nickname)
+    ) {
       setEditValidateState({ ...editValidateState, nickname: false });
       setNicknameValidText('invalidate');
-    } else if (event.target.type === 'text' && nicknameExp.test(userInputInfo.nickname)) {
+    } else if (
+      event.target.type === 'text' &&
+      nicknameExp.test(userInputInfo.nickname)
+    ) {
       axios
-        .post('/api/users/nickname', { nickname: userInputInfo.nickname }, config)
+        .post(
+          '/api/users/nickname',
+          { nickname: userInputInfo.nickname },
+          config
+        )
         .then((res) => {
           //사용가능한 닉네임인 경우
           setEditValidateState({ ...editValidateState, nickname: true });
@@ -116,33 +148,76 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
 
   const PasswordOnBlurHandler = (event) => {
     //password OnBlur
-    if (event.target.type === 'password' && userInputInfo.password === '' && event.target.placeholder === 'New Password') {
+    if (
+      event.target.type === 'password' &&
+      userInputInfo.password === pwCheckdValue &&
+      event.target.placeholder === 'New Password'
+    ) {
+      setEditValidateState({ ...editValidateState, password: false });
+      setPwValidText('same');
+    } else if (
+      event.target.type === 'password' &&
+      userInputInfo.password === '' &&
+      event.target.placeholder === 'New Password'
+    ) {
       setEditValidateState({ ...editValidateState, password: false });
       setPwValidText('nothing');
-    } else if (event.target.type === 'password' && !pwdExp.test(userInputInfo.password) && event.target.placeholder === 'New Password') {
+    } else if (
+      event.target.type === 'password' &&
+      !pwdExp.test(userInputInfo.password) &&
+      event.target.placeholder === 'New Password'
+    ) {
       setEditValidateState({ ...editValidateState, password: false });
       setPwValidText('invalidate');
-    } else if (event.target.type === 'password' && pwdExp.test(userInputInfo.password) && event.target.placeholder === 'New Password') {
+    } else if (
+      event.target.type === 'password' &&
+      pwdExp.test(userInputInfo.password) &&
+      event.target.placeholder === 'New Password'
+    ) {
       setEditValidateState({ ...editValidateState, password: true });
       setPwValidText('ok');
     }
 
     //password Check OnBlur
-    if (event.target.type === 'password' && userInputInfo.passwordCheck === '' && event.target.placeholder === 'Password Check') {
+    if (
+      event.target.type === 'password' &&
+      userInputInfo.passwordCheck === '' &&
+      event.target.placeholder === 'Password Check'
+    ) {
       setEditValidateState({ ...editValidateState, passwordCheck: false });
       setPwCheckValidText('nothing');
-    } else if (event.target.type === 'password' && userInputInfo.passwordCheck !== '' && event.target.placeholder === 'Password Check' && userInputInfo.password !== userInputInfo.passwordCheck) {
+    } else if (
+      event.target.type === 'password' &&
+      userInputInfo.passwordCheck !== '' &&
+      event.target.placeholder === 'Password Check' &&
+      userInputInfo.password !== userInputInfo.passwordCheck
+    ) {
       setEditValidateState({ ...editValidateState, passwordCheck: false });
       setPwCheckValidText('invalidate');
-    } else if (event.target.type === 'password' && userInputInfo.passwordCheck !== '' && event.target.placeholder === 'Password Check' && userInputInfo.password === userInputInfo.passwordCheck) {
+    } else if (
+      event.target.type === 'password' &&
+      userInputInfo.passwordCheck !== '' &&
+      event.target.placeholder === 'Password Check' &&
+      userInputInfo.password === userInputInfo.passwordCheck
+    ) {
       setEditValidateState({ ...editValidateState, passwordCheck: true });
       setPwCheckValidText('ok');
     }
   };
 
   const editHandler = (event) => {
-    if (allValidateCheck) {
-      //axios 요청
+    if (userInputInfo.nickname === '' && userInputInfo.password === '') {
+      setNicknameValidText('nothing');
+      setPwValidText('nothing');
+    } else if (
+      editValidateState.nickname ||
+      (editValidateState.password && editValidateState.passwordCheck)
+    ) {
+      if (editValidateState.password && editValidateState.passwordCheck) {
+        userInputInfo.password = userInputInfo.password;
+      } else {
+        userInputInfo.password = pwCheckdValue;
+      }
       axios
         .patch(
           '/api/users/profile',
@@ -153,40 +228,57 @@ const UserEditModal = ({ openUserEditModalHandler }) => {
           config2
         )
         .then((res) => {
-          localStorage.setItem('userInfo', JSON.stringify({ ...JSON.parse(localStorage.userInfo), nickname: userInputInfo.nickname }));
+          localStorage.setItem(
+            'userInfo',
+            JSON.stringify({
+              ...JSON.parse(localStorage.userInfo),
+              nickname: userInputInfo.nickname,
+            })
+          );
           alert('회원정보 수정이 완료되었습니다.');
           openUserEditModalHandler();
         })
         .catch((err) => {
           console.log(err);
         });
-    } else {
-      //입력 안한것이 있으면 필수 입력이라고 문구 나와야됨.
-      if (!nicknameValidateMessage) {
-        setNicknameValidText('nothing');
-      }
-      if (!pwValidateMessage) {
-        setPwValidText('nothing');
-      }
-      if (!pwCheckValidateMessage) {
-        setPwCheckValidText('nothing');
-      }
     }
   };
 
   return (
-    <>
+    <UserEditModalWrap onClick={(e) => e.stopPropagation()}>
       <h2>UserEditModal</h2>
       <button onClick={openUserEditModalHandler}>Close</button>
       <div>회원 정보 수정</div>
-      <input type='text' placeholder='Nickname' value={userInputInfo.nickname} onChange={editInputValueChange} onBlur={editUpOnBlurHandler} />
-      {nicknameValidateMessage ? <span>{nicknameValidateMessage}</span> : <br />}
-      <input type='password' placeholder='New Password' value={userInputInfo.password} onChange={editInputValueChange} onBlur={PasswordOnBlurHandler} />
+      <input
+        type='text'
+        placeholder='Nickname'
+        value={userInputInfo.nickname}
+        onChange={editInputValueChange}
+        onBlur={editUpOnBlurHandler}
+      />
+      {nicknameValidateMessage ? (
+        <span>{nicknameValidateMessage}</span>
+      ) : (
+        <br />
+      )}
+      <input
+        type='password'
+        placeholder='New Password'
+        value={userInputInfo.password}
+        onChange={editInputValueChange}
+        onBlur={PasswordOnBlurHandler}
+      />
       {pwValidateMessage ? <span>{pwValidateMessage}</span> : <br />}
-      <input type='password' placeholder='Password Check' value={userInputInfo.passwordCheck} onChange={editInputValueChange} onBlur={PasswordOnBlurHandler} />
+      <input
+        type='password'
+        placeholder='Password Check'
+        value={userInputInfo.passwordCheck}
+        onChange={editInputValueChange}
+        onBlur={PasswordOnBlurHandler}
+      />
       {pwCheckValidateMessage ? <span>{pwCheckValidateMessage}</span> : <br />}
       <button onClick={editHandler}>수 정</button>
-    </>
+    </UserEditModalWrap>
   );
 };
 
