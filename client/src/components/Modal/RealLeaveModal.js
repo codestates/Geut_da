ca48c02d05786265fa899e0144e6bc50f3346ea2
developerjhp <developerjhp@gmail.com>
2021-11-16 15:52:47 +0900

    [Feat] 회원탈퇴시 이미지s3삭제 closed #132

--- client/src/components/Modal/RealLeaveModal.js
+++ client/src/components/Modal/RealLeaveModal.js
@@ -3,6 +3,10 @@ import IsLoginState from '../../states/IsLoginState';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { LANDING } from '../../constants/routes';
+import dotenv from 'dotenv';
+import S3 from 'react-aws-s3';
+
+dotenv.config();
 
 const RealLeavModalWrap = styled.div`
   display: flex;
@@ -14,22 +18,29 @@ const RealLeavModalWrap = styled.div`
 `;
 
 const RealLeaveModal = ({ isRealUserResignHandler }) => {
+  const config = {
+    bucketName: process.env.REACT_APP_BUCKET_NAME,
+    region: process.env.REACT_APP_REGION,
+    accessKeyId: process.env.REACT_APP_ACCESS_ID,
+    secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
+  };
+  const ReactS3Client = new S3(config);
   const [isLogin, setIsLogin] = useRecoilState(IsLoginState);
   const realResignHandler = () => {
     const config = {
       headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem('userInfo')).token
-        }`,
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem('userInfo')).token}`,
       },
     };
     axios
       .delete('/api/users/profile', config)
       .then((res) => {
-        alert('탈퇴되었습니다. 이용해주셔서 감사합니다.');
-        setIsLogin(false);
-        localStorage.removeItem('userInfo');
-        window.location.replace(LANDING);
+        ReactS3Client.deleteFile(JSON.parse(localStorage.getItem('userInfo')).image.split('/')[3]).then((res) => {
+          alert('탈퇴되었습니다. 이용해주셔서 감사합니다.');
+          setIsLogin(false);
+          localStorage.removeItem('userInfo');
+          window.location.replace(LANDING);
+        });
       })
       .catch((err) => {
         console.log(err);
