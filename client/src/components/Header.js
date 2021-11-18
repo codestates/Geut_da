c03cc81e8cadcfae2f5b4cbb0f1629cd53e29161
linehyang <83056347+linehyang@users.noreply.github.com>
2021-11-18 10:56:49 +0900

    [Feat] #176 DrawingModal 문구 추가 (#177)
    - DrawingModal 설명 문구 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,10 +9,6 @@ const HeaderWrap = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  @media screen and (max-width: 768px) {
-    display: none;
-    background-color: red;
-  }
 `;
 
 const Logo = styled.div`
